2f3e6bde75cccbaad4c64c1458c6a85161a11d1b
Haresh Chander <engrhareshchander@gmail.com>
2025-12-05 14:00:54 +0500

    Update AoC_Part2.py

--- Advent of Code Day 4/AoC_Part2.py
+++ Advent of Code Day 4/AoC_Part2.py
@@ -1,1 +1,42 @@
+grid = []
+with open("sample_input_4.txt") as f:
+    for line in f:
+        line = line.strip()
+        if line != "":
+            grid.append(list(line))
 
+rows = len(grid)
+cols = len(grid[0])
+
+directions = [
+    (-1,-1), (-1,0), (-1,1),
+    (0,-1),          (0,1),
+    (1,-1),  (1,0),  (1,1)
+]
+
+total_removed = 0
+
+while True:
+    to_remove = []
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == '@':
+                adj = 0
+                for dr, dc in directions:
+                    nr = r + dr
+                    nc = c + dc
+                    if 0 <= nr < rows and 0 <= nc < cols:
+                        if grid[nr][nc] == '@':
+                            adj += 1
+                if adj < 4:
+                    to_remove.append((r, c))
+
+    if not to_remove:
+        break
+
+    for r, c in to_remove:
+        grid[r][c] = '.'
+        total_removed += 1
+
+print(total_removed)
